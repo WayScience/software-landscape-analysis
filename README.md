e24d57203108d1fa72564bb1514785024854d5ca
Dave Bunten <dave.bunten@cuanschutz.edu>
2023-10-10 16:52:37 -0600

    Initialize repository and Github data gathering efforts (#1)
    * add poetry config
    
    * expand readme
    
    * add linting checks
    
    * add initial data
    
    * updates; add ymlformat
    
    * lint yaml formatting
    
    * yaml formatting
    
    * add pytz as an explicit dependency
    
    * add initial git metrics notebook
    
    * linting
    
    * add isort config
    
    * Update README.md
    
    Co-authored-by: Gregory Way <gregory.way@gmail.com>
    
    * print project names
    
    * lint readme
    
    * execution for notebook
    
    * relabel for cytomining ecosystem
    
    ---------
    
    Co-authored-by: Gregory Way <gregory.way@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# landscape-analysis
-Enabling landscape analysis to better understand existing efforts or solutions.
+# Landscape Analysis
+
+A "landscape analysis" is an informational survey of existing software and other tooling that solve similar problems or use similar technologies.
+This repository performs landscape analyses for specific projects.
+
+## Installation
+
+Please use Python [`poetry`](https://python-poetry.org/) to run and install related content.
+
+```bash
+# after installing poetry, create the environment
+poetry install
+```
+
+## Development
+
+Please follow installation steps above and then use a relevant Jupyter environment to open and explore the notebooks under the `notebooks` directory.
+
+```bash
+# after creating poetry environment, run jupyter
+poetry run jupyter lab
+```
---
+++ notebooks/cytomining-ecosystem/project-git-metrics.py
@@ -0,0 +1,89 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.15.2
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# # Project Git Metrics for Landscape Analysis
+#
+# Project git metrics for software landscape analysis related to Cytomining ecosystem.
+#
+# ## Setup
+#
+# Set an environment variable named `LANDSCAPE_ANALYSIS_GH_TOKEN` to a [GitHub access token](https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens). E.g.: `export LANDSCAPE_ANALYSIS_GH_TOKEN=token_here`
+
+# +
+import os
+from datetime import datetime
+
+import pandas as pd
+import pytz
+from box import Box
+from github import Auth, Github
+
+# set github authorization and client
+github_client = Github(
+    auth=Auth.Token(os.environ.get("LANDSCAPE_ANALYSIS_GH_TOKEN")), per_page=100
+)
+# get the current datetime
+tz = pytz.timezone("UTC")
+current_datetime = datetime.now(tz)
+
+# +
+# gather projects data
+projects = Box.from_yaml(filename="data/projects.yaml").projects
+
+# check the number of projects
+print("number of projects: ", len(projects))
+print("project names: ", [project["name"] for project in projects])
+# -
+
+# show the keys available for the projects
+projects[0].keys()
+
+# +
+df_projects = pd.DataFrame(
+    # create a list of repo data records for a dataframe
+    [
+        {
+            "Project Name": repo.name,
+            "GitHub Stars": repo.stargazers_count,
+            "GitHub Forks": repo.forks_count,
+            "GitHub Watchers": repo.subscribers_count,
+            "GitHub Open Issues": repo.get_issues(state="open").totalCount,
+            "GitHub Contributors": repo.get_contributors().totalCount,
+            "GitHub License Type": repo.get_license().license.spdx_id,
+            "Date Created": repo.created_at.replace(tzinfo=pytz.UTC),
+            "Date Most Recent Commit": repo.get_commits()[0].commit.author.date.replace(
+                tzinfo=pytz.UTC
+            ),
+            "Duration Created to Most Recent Commit": "",
+            "Duration Most Recent Commit to Now": "",
+            "Repository Size (KB)": repo.size,
+        }
+        # make a request for github repo data with pygithub
+        for repo in [
+            github_client.get_repo(project.repo_url.replace("https://github.com/", ""))
+            for project in projects
+        ]
+    ]
+)
+
+# calculate time deltas
+df_projects["Duration Created to Most Recent Commit"] = (
+    df_projects["Date Most Recent Commit"] - df_projects["Date Created"]
+)
+df_projects["Duration Most Recent Commit to Now"] = (
+    current_datetime - df_projects["Date Most Recent Commit"]
+)
+
+# show the result
+df_projects
